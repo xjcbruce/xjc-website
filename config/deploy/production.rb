be935230aa67c7049bcc53135a352aac2860b4a2
xjcbruce <jiechangtech@gmail.com>
2017-02-10 19:03:05 +0800

    for new server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "120.25.204.36", user: "deploy", roles: %w{app db web}
+server "120.25.208.21", user: "deploy", roles: %w{app db web}
 
 # role-based syntax
 # ==================
